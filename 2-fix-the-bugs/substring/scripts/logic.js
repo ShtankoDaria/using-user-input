b51722149422e54456eb02ce9b0b1822545f3dcf
Daria <daria.shtanko@gmail.com>
2020-02-13 18:53:11 +0100

    Changes

--- 2-fix-the-bugs/substring/scripts/logic.js
+++ 2-fix-the-bugs/substring/scripts/logic.js
@@ -1,15 +1,21 @@
 // the tests are fine!
 const getPartialStringTests = [
-  { name: 'first', args: ['tall', 4, 5], expected: '' },
-  { name: 'second', args: ['888()888', 1, 3], expected: '88(' },
-  { name: 'third', args: ['falling trees', 0, 4], expected: 'fall' },
-  { name: 'fourth', args: ['mangrove', 1, 5], expected: 'angro' },
-  { name: 'fifth', args: ['everafter', -1, 2], expected: 'r' },
+  { name: "first", args: ["tall", 4, 5], expected: "" },
+  { name: "second", args: ["888()888", 1, 3], expected: "88(" },
+  { name: "third", args: ["falling trees", 0, 4], expected: "fall" },
+  { name: "fourth", args: ["mangrove", 1, 5], expected: "angro" },
+  { name: "fifth", args: ["everafter", -1, 2], expected: "r" }
 ];
 
 // fix the function
 function getPartialString(str, beginIndex, endIndex) {
-  return str.substring(beginIndex, endIndex);
+  if (beginIndex == 0) {
+    return str.substring(beginIndex, endIndex);
+  } else if (beginIndex < 0) {
+    return str.substring(str.length + beginIndex);
+  } else {
+    return str.substring(beginIndex, endIndex + 1);
+  }
 }
 
 testing(getPartialString, getPartialStringTests);
