8214b1c11a265e5678c7e9128db3a3906526467e
Daria <daria.shtanko@gmail.com>
2020-02-13 19:10:09 +0100

    Done, huh

--- 2-fix-the-bugs/type-caster-2/scripts/handlers.js
+++ 2-fix-the-bugs/type-caster-2/scripts/handlers.js
@@ -2,21 +2,21 @@ function typeCastHandler(event) {
   // read & process user input
   const form = event.target.form;
   const intendedType = form.type.value;
-  const newValue = form.value.value;
+  const stringToCast = form.value.value;
 
   // execute core logic
-  const newValue = typeCasting(stringToCast, intendedType);
+  const newValue = typeCaster(stringToCast, intendedType);
 
   // communicate result to user
-  document.GetElementById('casted-value').InnerHTML = typeof newValue + ': ' + newValue;
+  document.getElementById("casted-value").InnerHTML =
+    typeof newValue + ": " + newValue;
 
   // log action for developers
-  console.log('\n-- user action --');
-  console.log('stringToCast:', '(' + typeof stringToCast + ')', stringToCast);
-  console.log('intendedType:', '(' + typeof intendedType + ')', intendedType);
-  console.log('newValue:', '(' + typeof newValue + ')', newValue);
+  console.log("\n-- user action --");
+  console.log("stringToCast:", "(" + typeof stringToCast + ")", stringToCast);
+  console.log("intendedType:", "(" + typeof intendedType + ")", intendedType);
+  console.log("newValue:", "(" + typeof newValue + ")", newValue);
 
   // tell the browser everything is OK
   return true;
 }
-
