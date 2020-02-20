1fef322358e646cde7c324ba05b78d54725919e9
Daria <daria.shtanko@gmail.com>
2020-02-20 13:46:52 +0100

    some addings

--- 3-develop-from-specifications/google-search/scripts/handlers.js
+++ 3-develop-from-specifications/google-search/scripts/handlers.js
@@ -1,16 +1,13 @@
-function handler(event) {
+function gooooogle(event) {
   // read & process user input
-
-
+  const UserSearch = event.target.form.query.value;
   // call the core logic
-
-
+  const variable = googleURL(searchQuery);
   // render results for the user
-
-
+  window.Open;
   // log action for developers
-  console.log('\n-- user action --');
-  console.log('variable:', '(' + typeof variable + ')', variable);
+  console.log("\n-- user action --");
+  console.log("variable:", "(" + typeof variable + ")", variable);
 
   // tell the browser everything is OK
   return true;
